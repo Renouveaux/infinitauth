cafd62759771fcf966a41040a29fa1af85aed39d
Renouveaux <renouveaux@hotmail.com>
2016-11-18 03:29:28 +0100

    Adding PUT route

--- routes/account.js
+++ routes/account.js
@@ -11,7 +11,7 @@ account.use(jwt({secret: process.env.secret}).unless(function(req) {
 	return (
 		req.method === 'PUT'
 		);
-	})
+})
 );
 
 account.use(function (err, req, res, next) {
@@ -40,8 +40,94 @@ account.get('/:account_id', function(req, res, next){
 });
 
 account.put('/', function(req, res, next){
-	res.send("Put ok.");
+
+	if(Object.keys(req.body.accounts).length <= 0){
+		res.status(405).json({"message": "Accounts data not send correctly"}); 
+		return false;
+	}
+
+	if(Object.keys(req.body).length <= 0) {
+		res.status(405).json({"message": "Thanks to give some data"}); 
+		return false;
+	}
+
+	User.findOne({ username: req.body.username }, function(err, user) {
+		if (err) {
+			res.status(400).json({"message": "an error occured, your request can not be processed"})
+			return false;
+		}
+
+		if (user) {
+			res.status(409).json({ message: 'This user account already exist' });
+			return false;
+		}
+
+		const admin = new User({
+			username: req.body.username || null,
+			firstname: req.body.firstname || null,
+			lastname: req.body.lastname || null,
+			email: req.body.email || null,
+			accounts: {
+				kind: req.body.accounts.kind || null,
+				password: req.body.accounts.password || null
+			}
+		});
+
+		return admin.save(function(saveErr) {
+			if (saveErr){
+				res.status(400).json({"message": saveErr.errors[Object.keys(saveErr.errors)[0]].message});
+				return false;
+			};
+
+			return res.json({ "message": "Your account was created" });
+		});
+	});
+
+});
+
+account.patch('/:account_id', function(req, res, next){
+
+	res.status(405).json({"message": "Your not allowed to update this resource"});
+
+	res.status(204).end();
+
+});
+
+account.delete('/:account_id', function(req, res, next){
+
+	res.status(405).json({"message": "Your not allowed to delete this resource"});
+
+	res.status(204).end();
+
 });
 
 
 module.exports = account;
+
+/*auth.get('/setup', function(req, res) {
+  User.findOne({ username: 'admin' }, function(err, user) {
+    if (err) throw err;
+
+    if (user) {
+      return res.status(409).json({ message: 'user already exists' });
+    }
+
+    const admin = new User({
+      username: 'admin',
+      firstname: 'admin',
+      lastname: 'admin',
+      email: `admin@${process.env.MONGO_ADDR}`,
+      accounts: {
+      	kind: 'internal',
+      	password: 'changemeplease'
+      }
+    });
+
+    return admin.save(function(saveErr) {
+      if (saveErr) throw saveErr;
+
+      return res.json({ success: true });
+    });
+  });
+});
+*/
